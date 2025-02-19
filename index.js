473298f9c79bccd001db08a9346233a616e19e05
ScrachStack <122732007+ScrachStack@users.noreply.github.com>
2025-02-19 07:15:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -107,3 +107,54 @@ app.get('/' + URLS.LicenseCheck, (req, res) => {
 app.listen(config.port, () => {
     console.log('Server is listening on port ' + config.port);
 });
+if (config.bot.enabled) {
+// Discord Bot
+const { Client, GatewayIntentBits, ActivityType, Collection, REST, Routes } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+client.commands = new Collection();
+const loadCommands = async () => {
+    const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
+
+    const commands = [];
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.data.name, command);
+        commands.push(command.data);
+    }
+    const rest = new REST({ version: '10' }).setToken(token);
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+};
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    loadCommands(); 
+});
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+}
